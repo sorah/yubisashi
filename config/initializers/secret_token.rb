dd14bdebeb2aad9694d01edf0067695e24527673
Shota Fukumori (sora_h) <sorah@tubusu.net>
2011-07-13 11:10:19 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yubisashi::Application.config.secret_token = '4e81baf5287f4a24222aa91139e3c179a2739826b017183fc5fcacc4f394c9f18799ff2f9c943dcd04c803f8addbbec59d82dd62b5680913fcf2f9215d55bf22'
